ebe3bb94b1d1a17e0115ac9b80bebff15f5e448b
Rohit Raj <164794013+codeefy@users.noreply.github.com>
2026-04-03 01:39:40 +0530

    Time: 583 ms (91.5%), Space: 0B (100%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,3 +1,8 @@
 # Write your MySQL query statement below
 select user_id,concat(upper(substring(name,1,1)),lower(substring(name,2))) as name from users
 order by user_id
+
+-- substring(name,1,1) it will extract the first character of of name with lenght 1 
+-- then upper(substring(name,1,1,)) it will convert the first letter to upper case 
+-- then substring(name,2) it will extract the rest of the string except the first character then we will conver them using lower 
+-- then we will concat them using concat 
