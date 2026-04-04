53f23b7854bb36745a5f48f8050640c1dce28015
Rohit Raj <164794013+codeefy@users.noreply.github.com>
2026-04-05 01:03:36 +0530

    Update Page With No Likes.sql

--- Page With No Likes/Page With No Likes.sql
+++ Page With No Likes/Page With No Likes.sql
@@ -3,3 +3,10 @@ FROM pages p
 left outer join page_likes pl on p.page_id=pl.page_id
 where pl.page_id is NULL
 order by p.page_id;
+
+
+--- Alternative Solution using subquery 
+SELECT page_id
+FROM pages
+WHERE page_id NOT IN (SELECT page_id FROM page_likes)
+ORDER BY page_id;
