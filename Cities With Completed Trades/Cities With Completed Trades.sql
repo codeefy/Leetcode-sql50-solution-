100e472fb5c5e08a69b61d747ce21d792529a356
Rohit Raj <164794013+codeefy@users.noreply.github.com>
2026-04-07 03:24:51 +0530

    Update Cities With Completed Trades.sql

--- Cities With Completed Trades/Cities With Completed Trades.sql
+++ Cities With Completed Trades/Cities With Completed Trades.sql
@@ -1,1 +1,10 @@
+SELECT users.city, 
+  COUNT(trades.order_id) AS total_orders
+FROM trades 
+JOIN users
+ON trades.user_id = users.user_id 
+WHERE trades.status = 'Completed'
+GROUP BY users.city
+order by total_orders DESC
+limit 3;
 
