f88d66309091cbcc57a527d2d9f5aa4930e8b065
Rohit Raj <164794013+codeefy@users.noreply.github.com>
2026-04-07 00:07:46 +0530

    Refactor SQL query for non-evil wands selection

--- Ollivander's Inventory/Ollivander's Inventory.sql
+++ Ollivander's Inventory/Ollivander's Inventory.sql
@@ -1,15 +1,15 @@
 select w.id,wp.age,w.coins_needed,w.power from wands w 
 join wands_property wp on w.code=wp.code
 WHERE 
-    wp.is_evil = 0               -- only non-evil wands
-    AND w.coins_needed = (       --only minimum coins
+    wp.is_evil = 0               
+    AND w.coins_needed = (   
         SELECT MIN(w2.coins_needed)
         FROM Wands w2
         JOIN Wands_Property wp2 ON w2.code = wp2.code
-        WHERE w2.power = w.power -- same power
-        AND wp2.age = wp.age     -- same age
-        AND wp2.is_evil = 0      -- also non-evil
+        WHERE w2.power = w.power 
+        AND wp2.age = wp.age     
+        AND wp2.is_evil = 0      
     )
 ORDER BY 
-    w.power DESC,                -- highest power first
-    wp.age DESC                  -- if same power, older first
+    w.power DESC,
+    wp.age DESC                 
