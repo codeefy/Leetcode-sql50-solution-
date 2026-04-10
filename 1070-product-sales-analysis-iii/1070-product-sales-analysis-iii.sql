3c794a3c1f0ff3d7164a4ef780ba38b1fa7ef3e5
Rohit Raj <164794013+codeefy@users.noreply.github.com>
2026-04-11 01:16:59 +0530

    Time: 666 ms (77.83%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -3,3 +3,5 @@ select product_id,year as first_year ,quantity,price from sales
 where (product_id,year) in (select product_id,min(year) as year from sales 
 group by product_id
 )
+
+-- revise concept of subquery , also partitions , and how to solve with clasue 
