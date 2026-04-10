221d9d8d8d0a0dcfae668822a58d8af24716cf25
Rohit Raj <164794013+codeefy@users.noreply.github.com>
2026-04-11 00:43:56 +0530

    Add query to count messages by sender for August 2022

--- Teams Power Users Microsoft SQL Interview Question/Teams Power Users Microsoft SQL Interview Question.sql
+++ Teams Power Users Microsoft SQL Interview Question/Teams Power Users Microsoft SQL Interview Question.sql
@@ -1,1 +1,6 @@
-
+select sender_id ,count(*) as message_count
+from messages 
+where sent_date BETWEEN '08/01/2022 00:00:00' and '08/30/2022 23:59:59'
+group by sender_id
+order by message_count DESC
+limit 2
